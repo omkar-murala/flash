b32a34a0a467acc6eb383f3a56823988b9fbab6f
omkar-murala <omkar.developer1579@gmail.com>
2024-08-29 17:07:41 +0530

    all done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,10 +6,23 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Flash", // Replace with your website title
-  description: "Your Website Description", // Replace with your website description
+  title: "Flash | Helping Indian Brand Scale Globally | Flash Checkout for 30+ Countries |", // Replace with your website title
+  description: "Borderless Commerce", // Replace with your website description
   icons: {
     icon: "/fav.png", // Replace with your favicon, should be in the public directory
+  },
+  openGraph: {
+    title: "Flash | Helping Indian Brand Scale Globally",
+    description: "Flash Checkout for 30+ Countries",
+    images: [
+      {
+        url: "/fav.png", // Path to your resized image in the public directory
+        width: 600, // Set your preferred width
+        height: 315, // Set your preferred height
+        alt: "Flash Checkout",
+      },
+    ],
+    url: "https://www.goflash.in",
   },
 };
 
